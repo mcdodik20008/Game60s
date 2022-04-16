5955c41dd6a78f34b95c2fed2fd0196737be00f8
Алексей Каспшицкий <stud0000247033@study.utmn.ru>
2022-04-16 20:16:49 +0500

    Добавьте файлы проекта.

---
+++ Game60s/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game60s
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            object x = 1;
+            if  (x is var x2)
+            {
+                Console.WriteLine(x2);
+            }
+        }
+    }
+}
